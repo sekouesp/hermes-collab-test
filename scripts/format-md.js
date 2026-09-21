fcd746f9f66bc04128f1638e5b1f673a929b0f93
Pseudo Line <thepseudoline@gmail.com>
2026-09-21 17:09:07 +0000

    Add scripts/format-md.js

---
+++ scripts/format-md.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+/**
+ * Simple markdown formatter: read stdin, fix line endings, write stdout.
+ */
+const { readFileSync, writeFileSync } = require('fs');
+
+const input = readFileSync(0, 'utf-8');
+const formatted = input
+  .replace(/
+/g, '
+')
+  .replace(/
+{3,}/g, '
+
+')
+  .trim() + '
+';
+
+process.stdout.write(formatted);
